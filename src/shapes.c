a93bb2e245cf16791baa019bedf54c8c7081f348
mkupiec1 <mkupiec@o2.pl>
2021-03-31 16:49:25 +0200

    w

--- src/shapes.c
+++ src/shapes.c
@@ -1555,7 +1555,7 @@ bool CheckCollisionPointLine(Vector2 point, Vector2 p1, Vector2 p2, int threshol
     float dyl = p2.y - p1.y;
     float cross = dxc*dyl - dyc*dxl;
     
-    if (abs(cross) < threshold*fmaxf(fabsf(dxl), fabsf(dyl)))
+    if (fabsf(cross) < threshold*fmaxf(fabsf(dxl), fabsf(dyl)))
     {
         if (fabsf(dxl) >= fabsf(dyl)) collision = (dxl > 0)? ((p1.x <= point.x) && (point.x <= p2.x)) : ((p2.x <= point.x) && (point.x <= p1.x));
         else
